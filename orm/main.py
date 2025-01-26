80d203371fe873b9ba88c27f04631e114e06a9ce
Abuzar <abuzarkhan1242@gmail.com>
2025-01-27 01:34:37 +0500

    orm Alchemy

---
+++ orm/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI,Response,status,HTTPException, Depends
+from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine) 
+
+app = FastAPI()
+
+
+def get_db():
+    db = Session_local()
+    try:
+        yield db
+    finally:
+        db.close()    
+
+
+@app.get("/get")
+def read_root(db: Session = Depends(get_db)):
+    return {"status": "ok", }
+
